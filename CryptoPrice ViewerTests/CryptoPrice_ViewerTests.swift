d1f932b2a3cb194471d3a14c688ec0c001e228f9
dearestpankaj <dearestpankaj@gmail.com>
2020-08-08 17:06:05 +0530

    Initial Commit

---
+++ CryptoPrice ViewerTests/CryptoPrice_ViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  CryptoPrice_ViewerTests.swift
+//  CryptoPrice ViewerTests
+//
+//  Created by Pankaj Sachdeva on 08/08/20.
+//  Copyright © 2020 Pankaj Sachdeva. All rights reserved.
+//
+
+import XCTest
+@testable import CryptoPrice_Viewer
+
+class CryptoPrice_ViewerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
